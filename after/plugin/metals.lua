9b7dabd6ab44990bb9e9e8c8947ce8284169aa0a
Axel Ward <axelward771@gmail.com>
2024-01-16 09:04:52 -0700

    Add ftplugin

--- after/plugin/metals.lua
+++ after/plugin/metals.lua
@@ -12,12 +12,10 @@ metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
-  -- NOTE: You may or may not want java included here. You will need it if you
-  -- want basic Java support but it may also conflict if you are using
-  -- something like nvim-jdtls which also works on a java filetype autocmd.
   pattern = { "scala", "sbt", "java" },
   callback = function()
     require("metals").initialize_or_attach(metals_config)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   end,
   group = nvim_metals_group,
 })

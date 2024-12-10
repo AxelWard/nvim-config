d018fe88382f8f9e09f2667006fc775e00e979cc
Axel Ward <axelward771@gmail.com>
2024-12-10 15:32:48 -0700

    Add leader to beginning of config

--- lua/axelward/remap.lua
+++ lua/axelward/remap.lua
@@ -1,6 +1,5 @@
 require("axelward.insert_comments")
 
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>vcf", function() vim.lsp.buf.format() end)
 vim.keymap.set("n", "<leader>cn", function() CreateNote() end)

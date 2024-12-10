a5eb66cd29a2a4de8baf2226e323dadeabf3756c
Axel Ward <axelward771@gmail.com>
2024-12-10 15:31:16 -0700

    Add remap to other places

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,6 @@
 local builtin = require('telescope.builtin')
 
+vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
